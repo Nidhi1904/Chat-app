3586bc0b7e1712bba0817b4cdff119ff0473d378
Nidhi1904 <81015886+Nidhi1904@users.noreply.github.com>
2023-05-25 12:23:29 +0530

    Update Chats.jsx

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -33,12 +33,12 @@ const Chats = () => {
       {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
         <div
           className="userChat"
-          key={chat[0]}
-          onClick={() => handleSelect(chat[1].userInfo)}
+          key={chat[0]} //uid
+          onClick={() => handleSelect(chat[1].userInfo)} //contains all other info like uid, date, userInfo, length
         >
           <img src={chat[1].userInfo.photoURL} alt="" />
           <div className="userChatInfo">
-            <span>{chat[1].userInfo.displayName}</span>
+            <span>{chat[1].userInfo.displayName}</span> 
             <p>{chat[1].lastMessage?.text}</p>
           </div>
         </div>
